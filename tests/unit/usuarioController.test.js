205fa0f9619701ee44f27232d3d9a009f9af736b
Agustin Succar <73540@sistemas.frc.utn.edu.ar>
2023-08-11 11:31:37 -0300

    .

--- tests/unit/usuarioController.test.js
+++ tests/unit/usuarioController.test.js
@@ -1,3 +1,4 @@
+// Importa las funciones necesarias desde el módulo usuarioController
 const {
   getAllUsuarios,
   createUsuario,
@@ -5,26 +6,31 @@ const {
   deleteUsuario,
   getUsuarioById,
 } = require("../../src/controllers/usuarioController");
+
+// Importa el modelo Usuario desde el módulo usuarioModel
 const Usuario = require("../../src/models/usuarioModel");
 
+// Simula el modelo Usuario
 jest.mock("../../src/models/usuarioModel");
 
-describe("Usuario Controller", () => {
-  //Se declara un objeto mockRes en el que se simulan los metodos status y json
+// Bloque describe para el Controlador de Usuario
+describe("Controlador de Usuario", () => {
+  // Declara un objeto mockRes para simular los métodos status y json
   let mockRes;
   beforeEach(() => {
     mockRes = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
+      status: jest.fn().mockReturnThis(), // Simula el método status
+      json: jest.fn(), // Simula el método json
     };
   });
 
-  //Se definen diferentes pruebas utilizando la función test para cada función del controlador.
+  // Prueba para la función getAllUsuarios
   test("getAllUsuarios debería obtener todos los usuarios", async () => {
     const mockUsuarios = [
       { id: "1", name: "Libro 1" },
       { id: "2", name: "Libro 2" },
     ];
+    // Simula el método find del modelo Usuario para devolver el array mockUsuarios
     Usuario.find.mockResolvedValue(mockUsuarios);
     const mockReq = {};
     await getAllUsuarios(mockReq, mockRes);
@@ -32,12 +38,14 @@ describe("Usuario Controller", () => {
     expect(mockRes.json).toHaveBeenCalledWith(mockUsuarios);
   });
 
-  test("createUsuario debería crear un nuevo libro", async () => {
+  // Prueba para la función createUsuario
+  test("createUsuario debería crear un nuevo usuario", async () => {
     const mockUsuario = {
       id: "1",
       name: "Lucas Perez",
       email: "lucasperez@gmail.com",
     };
+    // Simula el método create del modelo Usuario para devolver el objeto mockUsuario
     mockUsuario.save = () => {};
     Usuario.create.mockResolvedValue(mockUsuario);
     const mockReq = { body: mockUsuario };
@@ -46,36 +54,44 @@ describe("Usuario Controller", () => {
     expect(mockRes.json).toHaveBeenCalledWith(mockUsuario);
   });
 
-  test("deleteUsuario debería eliminar un libro", async () => {
+  // Prueba para la función deleteUsuario
+  test("deleteUsuario debería eliminar un usuario", async () => {
     const mockUsuario = {
       id: "1",
       name: "Lucas Perez",
       email: "lucasperez@gmail.com",
     };
+    // Simula el método findByIdAndRemove del modelo Usuario para devolver el objeto mockUsuario
     Usuario.findByIdAndRemove.mockResolvedValue(mockUsuario);
     const mockReq = { params: { id: "1" } };
     await deleteUsuario(mockReq, mockRes);
     expect(mockRes.status).toHaveBeenCalledWith(200);
     expect(mockRes.json).toHaveBeenCalledWith(mockUsuario);
   });
+
+  // Prueba para la función getUsuarioById
   test("getUsuarioById debería obtener los detalles de un usuario", async () => {
     const mockUsuario = {
       id: "1",
       name: "Lucas Perez",
       email: "lucasperez@gmail.com",
     };
+    // Simula el método findById del modelo Usuario para devolver el objeto mockUsuario
     Usuario.findById.mockResolvedValue(mockUsuario);
     const mockReq = { params: { id: "1" } };
     await getUsuarioById(mockReq, mockRes);
     expect(mockRes.status).toHaveBeenCalledWith(200);
     expect(mockRes.json).toHaveBeenCalledWith(mockUsuario);
   })
+
+  // Prueba para la función updateUsuario
   test("updateUsuario debería actualizar la información de un usuario", async () => {
     const mockUsuario = {
       id: "1",
       name: "Lucas Perez",
       email: "lucasperez@gmail.com",
     };
+    // Simula el método findByIdAndUpdate del modelo Usuario para devolver el objeto mockUsuario
     Usuario.findByIdAndUpdate.mockResolvedValue(mockUsuario);
     const mockReq = { params: { id: "1" }, body: mockUsuario };
     await updateUsuario(mockReq, mockRes);
